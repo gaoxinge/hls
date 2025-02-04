99ec46f3406ca620a39286f353c84289d0ddae12
gaoxinge <gaoxx5@gmail.com>
2025-02-04 16:37:43 +0800

    [feat] support aes decode

--- hls/__init__.py
+++ hls/__init__.py
@@ -1,11 +1,14 @@
 import io
 import os
+from abc import ABC, abstractmethod
 from urllib.parse import urljoin
 from typing import Callable, IO, List
+from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
 
 import requests
+from Crypto.Cipher import AES
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
+
 
 Get = Callable[[str, IO], None]
 
@@ -24,6 +27,28 @@ class BS:
         return self.get_bytes(url).decode("utf-8")
 
 
+class Decoder(ABC):
+
+    @abstractmethod
+    def decode(self, content: bytes):
+        raise NotImplementedError
+
+
+class DefaultDecoder(Decoder):
+
+    def decode(self, content: bytes):
+        return content
+
+
+class AESDecoder(Decoder):
+
+    def __init__(self, key: bytes, iv: bytes):
+        self.aes = AES.new(key=key, IV=iv, mode=AES.MODE_CBC)
+
+    def decode(self, content: bytes):
+        return self.aes.decrypt(content)
+
+
 class Cache:
 
     def __init__(self, directory: str, get1: Get):
@@ -38,34 +63,91 @@ class Cache:
         with open(file_path, "wb") as f:
             self.get1(url, f)
 
-    def merge(self, url_list: List[str], file_path: str):
+    def merge(self, url_list: List[str], file_path: str, decoder: Decoder):
         with open(file_path, "wb") as f:
             for url in url_list:
                 _, url_suffix = url.rsplit("/", 1)
                 file_path1 = os.path.join(self.directory, url_suffix)
                 with open(file_path1, "rb") as g:
-                    f.write(g.read())
+                    f.write(decoder.decode(g.read()))
 
 
 class HLS:
 
-    def __init__(self, ts_url_list: List[str]):
-        self.ts_url_list = ts_url_list
+    EXT_X_KEY = "#EXT-X-KEY:"
+    EXT_X_KEY_METHOD = "METHOD"
+    EXT_X_KEY_URI = "URI"
+    EXT_X_KEY_IV = "IV"
+
+    def __init__(self):
+        self.ext_x_key = None
+        self.ext_x_key_enc_key = None
+        self.m3u8_url_list = None
+        self.ts_url_list = None
+
+    @staticmethod
+    def parse_ext_x_key(line: str):
+        line = line.split(HLS.EXT_X_KEY)[1]
+        result = {}
+        for line in line.split(","):
+            k, v = line.split("=")
+            result[k] = v
+        return result
 
     @staticmethod
     def parse(url: str, get: Get) -> 'HLS':
         bs = BS(get)
         url_prefix, _ = url.rsplit("/", 1)
-        result = bs.get_str(url)
-        m3u8_url_list = [urljoin(url_prefix, m3u8_name) for m3u8_name in result.split("\n")
-                         if not m3u8_name.startswith("#") and m3u8_name.endswith("m3u8")]
-        print(m3u8_url_list)
+        url_prefix += "/"
+
+        hls = HLS()
+        m3u8_url_list = []
         ts_url_list = []
+
+        result = bs.get_str(url)
+        for line in result.split("\n"):
+            if not line.startswith("#"):
+                if line.endswith("m3u8"):
+                    m3u8_url_list.append(urljoin(url_prefix, line))
+                if line.endswith("ts"):
+                    ts_url_list.append(urljoin(url_prefix, line))
+            else:
+                if line.startswith(HLS.EXT_X_KEY):
+                    hls.ext_x_key = HLS.parse_ext_x_key(line)
+                    if HLS.EXT_X_KEY_URI in hls.ext_x_key:
+                        hls.ext_x_key_enc_key = bs.get_bytes(
+                            urljoin(url_prefix, hls.ext_x_key[HLS.EXT_X_KEY_URI].replace("\"", ""))
+                        )
+                    if HLS.EXT_X_KEY_IV in hls.ext_x_key:
+                        hls.ext_x_key[HLS.EXT_X_KEY_IV] = bytes.fromhex(hls.ext_x_key[HLS.EXT_X_KEY_IV][2:])
+
         for m3u8_url in m3u8_url_list:
             result = bs.get_str(m3u8_url)
-            ts_url_list.extend([urljoin(url_prefix, ts_name) for ts_name in result.split("\n")
-                                if not ts_name.startswith("#") and ts_name.endswith("ts")])
-        return HLS(ts_url_list)
+            for line in result.split("\n"):
+                if not line.startswith("#"):
+                    if line.endswith("ts"):
+                        ts_url_list.append(urljoin(url_prefix, line))
+
+        hls.m3u8_url_list = m3u8_url_list
+        hls.ts_url_list = ts_url_list
+        return hls
+
+    def get_decoder(self) -> 'Decoder':
+        decoder = DefaultDecoder()
+        if self.ext_x_key is not None and HLS.EXT_X_KEY_METHOD in self.ext_x_key:
+            method = self.ext_x_key[HLS.EXT_X_KEY_METHOD]
+            if method == "AES-128":
+                decoder = AESDecoder(key=self.ext_x_key_enc_key, iv=self.ext_x_key[HLS.EXT_X_KEY_IV])
+        return decoder
+
+    def __str__(self):
+        return f"HLS(" \
+               f"ext_x_key={self.ext_x_key}, " \
+               f"ext_x_key_enc_key={self.ext_x_key_enc_key}, " \
+               f"m3u8_url_list={self.m3u8_url_list}, " \
+               f"ts_url_list={self.ts_url_list})"
+
+    __repr__ = __str__
 
 
 class MTDownloader:
@@ -88,8 +170,6 @@ class MTDownloader:
             f.write(response.content)
 
         self.hls = HLS.parse(url, getter)
-        for url in self.hls.ts_url_list:
-            print(url)
         self.cache = Cache(directory, getter)
         self.file_path = file_path
 
@@ -112,4 +192,4 @@ class MTDownloader:
                             raise e
                     total -= len(done)
                     bar.update(len(done))
-            self.cache.merge(self.hls.ts_url_list, self.file_path)
+        self.cache.merge(self.hls.ts_url_list, self.file_path, self.hls.get_decoder())
